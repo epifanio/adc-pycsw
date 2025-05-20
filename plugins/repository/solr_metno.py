cf9a6f0860cb7c3d2569625c560785966d27358f
epifanio <epiesasha@me.com>
2025-05-20 12:06:20 +0000

    renaming solr_mapping

--- plugins/repository/solr_metno.py
+++ plugins/repository/solr_metno.py
@@ -88,7 +88,7 @@ class SOLRMETNORepository(object):
         # self.config_obj = get_config()
         self.adc_collection_filter = get_collection_filter()
         # get the solr mapping for main queriebles
-        self.fields_dict = get_solr_mapping(repo_object.get("solr_mapping"))
+        self.solr_mapping = get_solr_mapping(repo_object.get("solr_mapping"))
         # print(self.adc_collection_filter)
 
         # generate core queryables db and obj bindings
@@ -116,7 +116,7 @@ class SOLRMETNORepository(object):
 
     def describe(self):
         """Derive table columns and types"""
-
+        LOGGER.debug("Running Describe on : %s", self.solr_mapping)
         # type_mappings = {"TEXT": "string", "VARCHAR": "string"}
         type_mappings = {
             "TEXT": "string",
@@ -135,7 +135,7 @@ class SOLRMETNORepository(object):
             }
         }
 
-        for i in self.fields_dict:
+        for i in self.solr_mapping:
             if i in ["anytext", "metadata", "metadata_type", "xml"]:
                 continue
 
@@ -145,8 +145,8 @@ class SOLRMETNORepository(object):
                 properties[i]["x-ogc-role"] = "id"
 
             try:
-                properties[i]["type"] = type_mappings[str(self.fields_dict[i])]
-                if self.fields_dict[i] == "pdate":
+                properties[i]["type"] = type_mappings[str(self.solr_mapping[i])]
+                if self.solr_mapping[i] == "pdate":
                     properties[i]["property"] = "date-time"
             except Exception as err:
                 # LOGGER.debug(f"Cannot determine type: {err}")
